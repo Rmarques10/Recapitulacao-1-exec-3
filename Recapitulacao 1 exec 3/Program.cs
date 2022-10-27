2d6511fab0358c492f36197ecb045e14a651db31
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 11:43:56 -0300

    Recapitulação

---
+++ Recapitulacao 1 exec 3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
+
+int a, b, c, d, diferenca;
+
+Console.WriteLine("Informe o valor de A:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor de A:");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor de A:");
+c = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor de A:");
+d = int.Parse(Console.ReadLine());
+
+diferenca = a * b - c * d;
+
+Console.WriteLine("Difenreça = " + diferenca);
